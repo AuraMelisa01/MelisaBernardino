0af0288ce0a91602a74aa03e39d996ac99b4906b
AuraMelisa01 <auramelisa6@gmail.com>
2020-04-24 10:25:05 -0500

    card botones

--- assets/js/nav-transparent.js
+++ assets/js/nav-transparent.js
@@ -2,10 +2,10 @@
 
 $(window).scroll(function() {
     if ($("#menu").offset().top > 53) {
-        $("#menu").addClass("back-black");
+        $("#menu").addClass("back-pink");
         $(".navbar-brand").addClass("black-color");
     } else {
-        $("#menu").removeClass("back-black");
+        $("#menu").removeClass("back-pink");
         $(".navbar-brand").removeClass("black-color");
     }
 });
