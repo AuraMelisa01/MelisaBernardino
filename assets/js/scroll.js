ed246ad2f615a7aba19695a896f3c1bc200cc512
AuraMelisa01 <auramelisa6@gmail.com>
2020-04-29 20:37:44 -0500

    stilos menu

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,15 +1,13 @@
-$('.ancla').on('click', function(e) {
-    e.preventDefault();
-    var strAncla = '#' + $(this).data('ancla');
-    var strAnclaHome = '#' + $(this).data('home');
-    var strAnclaMi = '#' + $(this).data('sobre-mi');
-    var strAnclaHabilidades = '#' + $(this).data('habilidades');
-    var strAnclaProyectos = '#' + $(this).data('proyectos');
-    var strAnclaContacto = '#' + $(this).data('contacto');
-    $('html,body').animate({ scrollTop: $(strAncla).offset().top }, 1000);
-    $('html,body').animate({ scrollTop: $(strAnclaHome).offset().top }, 1000);
-    $('html,body').animate({ scrollTop: $(strAnclaMi).offset().top }, 1000);
-    $('html,body').animate({ scrollTop: $(strAnclaHabilidades).offset().top }, 1000);
-    $('html,body').animate({ scrollTop: $(strAnclaProyectos).offset().top }, 1000);
-    $('html,body').animate({ scrollTop: $(strAnclaContacto).offset().top }, 1000);
+$(document).ready(function() {
+    $('a[href^="#"]').click(function() {
+        var destino = $(this.hash);
+        if (destino.length == 0) {
+            destino = $('a[name="' + this.hash.substr(1) + '"]');
+        }
+        if (destino.length == 0) {
+            destino = $('html');
+        }
+        $('html, body').animate({ scrollTop: destino.offset().top }, 1000);
+        return false;
+    });
 });
